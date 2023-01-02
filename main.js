8727990f689d0e1f82404fd76d7cba61749a940e
2011Karthik <100004622+2011Karthik@users.noreply.github.com>
2023-01-02 16:47:03 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-https://teachablemachine.withgoogle.com/models/mfdRdPjFX/
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/mfdRdPjFX/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
